55751377f97b5ec53cc070bbfdd455604657f384
Thom <47935791+Spebby@users.noreply.github.com>
2024-06-27 14:21:09 -0700

    Update DeadzoneVolume.cs
    Updated to use new features added in 2024's Vehicle Update preview branch.

--- DeadzoneVolume.cs
+++ DeadzoneVolume.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace SDG.Framework.Devkit
 {
@@ -6,5 +6,19 @@ namespace SDG.Framework.Devkit
     // BoxCollider must have 'isTrigger' set to True
     public class DeadzoneVolume : MonoBehaviour
     {
+        [SerializeField]
+		private EDeadzoneType _deadzoneType;
+	
+		[SerializeField]
+		private float _unprotectedDamagePerSecond;
+	
+		[SerializeField]
+		private float _protectedDamagePerSecond;
+	
+		[SerializeField]
+		private float _unprotectedRadiationPerSecond = 6.25f;
+	
+		[SerializeField]
+		private float _maskFilterDamagePerSecond = 0.4f;
     }
 }
