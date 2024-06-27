f3b02804a38a21bb67a6d2d215aeb6f58d8b4ad3
Thom <47935791+Spebby@users.noreply.github.com>
2024-06-27 14:22:10 -0700

    Update Acid.cs
    updated to have GUID support

--- Acid.cs
+++ Acid.cs
@@ -6,6 +6,10 @@ namespace SDG.Unturned
     // Destroys itself after one use and cannot be reset
     public class Acid : MonoBehaviour
     {
+        public Guid effectGuid;
+
+        // Kept because lots of modders have been using this script in Unity,
+		// so removing legacy effect id would break their content.
         public ushort effectID;
     }
 }
